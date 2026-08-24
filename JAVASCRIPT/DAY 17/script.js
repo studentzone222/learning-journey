693c8079caecc951c01864a9eb2848bab3b6770e
studentzone222 <studentzone222@gmail.com>
2026-08-24 19:11:09 +0600

    Day 17 ( learn = uppercase, lowercase, string formating , like white space removeing )

---
+++ JAVASCRIPT/DAY 17/script.js
@@ -0,0 +1,27 @@
+// To day i learn to upperCase lowercase
+// string white space 
+// how can i remove White space useing trim();
+
+
+const gettering = "Are you ok?"; // veriable declaration
+const upperCase = gettering.toUpperCase(); // convert the all veriable to Uppercase
+console.log(upperCase);  // print the veriable
+
+const impliment = "I wana see you"; // veriable declaration
+const lowerCase = impliment.toLowerCase(); // convert to the lowerCase
+console.log(lowerCase); // print the veriable
+
+console.log(impliment.toUpperCase()); // use console.log() convert the all value Uppercase
+
+const wheteSpace = "       Hello World!"; // use string the white space
+const message = wheteSpace.trim(); // remove the all white space
+console.log(wheteSpace); // print the white space 
+console.log(message); // print remove the white space
+
+
+const whitesp = wheteSpace.trimStart(); // start white space remove 
+console.log(whitesp);
+ 
+// remove end the sentance trimEnd();
+
+
