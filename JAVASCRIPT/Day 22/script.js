b52a456998f542776bd0c66d865c0209d184f0a1
studentzone222 <studentzone222@gmail.com>
2026-08-31 16:53:16 +0600

    DAY 21

---
+++ JAVASCRIPT/Day 22/script.js
@@ -0,0 +1,32 @@
+// Taday i will create a math Bot!
+const botName = "MathBot";
+const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
+
+console.log(greeting);
+
+console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
+
+const randomNum = Math.random();
+console.log(randomNum);
+
+console.log("Now, generate a random number between two values.");
+
+const min = 1;
+const max = 100;
+
+const randomNum2 = Math.random() * (max - min) + min;
+console.log(randomNum2);
+
+console.log("The Math.floor() method rounds the value down to the nearest whole integer.");
+
+const numRoundedDown = Math.floor(6.7);
+console.log(numRoundedDown);
+
+console.log("Now, generate a random integer between two values.");
+
+const randomInt = Math.floor(Math.random() * (max - min) + min);
+console.log(randomInt);
+
+console.log("The Math.ceil() method rounds the value up to the nearest whole integer.");
+
+
