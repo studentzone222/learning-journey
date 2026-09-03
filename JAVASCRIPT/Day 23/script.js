031aef6cd3aafbc3e106bedc7bd2ce2cbd403285
studentzone222 <studentzone222@gmail.com>
2026-09-03 14:31:26 +0600

    Switch Statement

---
+++ JAVASCRIPT/Day 23/script.js
@@ -0,0 +1,35 @@
+// Switch statement Use 7 day Remainder
+
+const day = 3;
+
+switch(day){
+    case 1:
+    console.log("It's Monday! Time to start the week strong.");
+    break;
+
+    case 2:
+    console.log("It's Tuesday! Keep the momentum going.");
+    break;
+
+    case 3:
+    console.log("It's Wednesday! We're halfway there.");
+    break;
+
+    case 4:
+    console.log("It's Thursday! Almost the weekend.");
+    break;
+
+      case 5:
+        console.log("It's Friday! The weekend is near.");
+        break;
+    case 6:
+        console.log("It's Saturday! Enjoy your weekend.");
+        break;
+    case 7:
+        console.log("It's Sunday! Rest and recharge.");
+        break;
+    default:
+    console.log("Invalid day! Please enter a number between 1 and 7.");
+}
+
+
